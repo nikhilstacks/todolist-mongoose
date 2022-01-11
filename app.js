ebc43e88cd369c4728ae5a4b5ad744a504a6f21b
nikhilstacks <nikhilstacks@gmail.com>
2022-01-11 15:08:01 +0530

    procfile

--- app.js
+++ app.js
@@ -163,6 +163,6 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(Process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
